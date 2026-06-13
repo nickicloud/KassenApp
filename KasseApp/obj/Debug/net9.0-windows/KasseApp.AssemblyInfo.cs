c53c0a99f81a4ad668cc4b7938caa4b7303153d3
NickiCloud <kniklas049@gmail.com>
2026-06-13 22:39:18 +0200

    Updater Code

--- KasseApp/obj/Debug/net9.0-windows/KasseApp.AssemblyInfo.cs
+++ KasseApp/obj/Debug/net9.0-windows/KasseApp.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Updater")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("KasseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c11cf7aae388196447111c032019d88e7ea20bf")]
-[assembly: System.Reflection.AssemblyProductAttribute("Updater")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Updater")]
+[assembly: System.Reflection.AssemblyProductAttribute("KasseApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("KasseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
